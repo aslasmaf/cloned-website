f1bf6cc455a1cff9ddbdf1d09200679ada1f4a87
aslasmaf <saifullahimuhdauwal@gmail.com>
2020-09-15 23:15:37 +0100

    ..

--- js/main.js
+++ js/main.js
@@ -18,4 +18,26 @@ function openNav() {
 
   }
 
+  //Mobile view slideshow 
+  var slideIndex = 1;
+  showSlides(slideIndex);
+  
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("m-v-Slides");
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slides[slideIndex-1].style.display = "block";  
+  }
  
