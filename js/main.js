8c5aae5433983f2cab98a039db041b05e1ec0900
aslasmaf <saifullahimuhdauwal@gmail.com>
2020-09-15 03:28:41 +0100

    ..

--- js/main.js
+++ js/main.js
@@ -1,41 +1,3 @@
-function display_slide_1(){
-    // Display the first slide
-    document.getElementById("s1").style.display='block';
-    document.getElementById("s2").style.display='none';
-    document.getElementById("s3").style.display='none';
-   //change the color
-    document.getElementById("s1sh").style.color='red';
-    document.getElementById("s2sh").style.color='rgb(150, 130, 130)';
-    document.getElementById("s3sh").style.color='rgb(150, 130, 130)';
-    document.getElementById("s1sp").style.color='#fff';
-    document.getElementById("s2sp").style.color='rgb(150, 130, 130)';
-    document.getElementById("s3sp").style.color='rgb(150, 130, 130)';
-}
-
-function display_slide_2(){
-    document.getElementById("s1").style.display='none';
-    document.getElementById("s2").style.display='block';
-    document.getElementById("s3").style.display='none';
-    document.getElementById("s2sh").style.color='red';
-    document.getElementById("s3sh").style.color='rgb(150, 130, 130)';
-    document.getElementById("s1sh").style.color='rgb(150, 130, 130)';
-    document.getElementById("s2sp").style.color='#fff';
-    document.getElementById("s3sp").style.color='rgb(150, 130, 130)';
-    document.getElementById("s1sp").style.color='rgb(150, 130, 130)';
-}
-
-function display_slide_3(){
-    document.getElementById("s1").style.display='none';
-    document.getElementById("s2").style.display='none';
-    document.getElementById("s3").style.display='block';
-    document.getElementById("s3sh").style.color='red';
-    document.getElementById("s2sh").style.color='rgb(150, 130, 130)';
-    document.getElementById("s1sh").style.color='rgb(150, 130, 130)';
-    document.getElementById("s3sp").style.color='#fff';
-    document.getElementById("s2sp").style.color='rgb(150, 130, 130)';
-    document.getElementById("s1sp").style.color='rgb(150, 130, 130)';
-}
-
 /* Set the width of the sidebar to 250px (show it) */
 function openNav() {
     document.getElementById("mySidepanel").style.width = "100%";
@@ -50,3 +12,10 @@ function openNav() {
     document.getElementById("mySidepanel").style.width = "0";
   } 
 
+  function slideAnimation(slide){
+    slideId = slide;
+    document.getElementById(slideId).style.animation="load-me 2000ms";
+
+  }
+
+ 
